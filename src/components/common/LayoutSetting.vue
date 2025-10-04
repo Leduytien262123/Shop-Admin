702bad677ce2bcd4b0e992695651979386bc0a29
Duy Tien <leduytien262@gmail.com>
2025-10-04 16:06:40 +0700

    refactor: Clean up LayoutSetting component code and improve formatting

--- src/components/common/LayoutSetting.vue
+++ src/components/common/LayoutSetting.vue
@@ -1,25 +1,29 @@
-<!--------------------------------
- - @Author: Ronnie Zhang
- - @LastEditor: Ronnie Zhang
- - @LastEditTime: 2023/12/16 18:49:53
- - @Email: zclzone@outlook.com
- - Copyright © 2023 Ronnie Zhang(大脸怪) | https://isme.top
- --------------------------------->
-
 <template>
   <div>
     <n-tooltip trigger="hover" placement="left">
       <template #trigger>
-        <div id="layout-setting" class="f-c-c rounded-4 bg-primary p-8" @click="modalRef.open()">
-          <i class="i-fe:settings cursor-pointer bg-white text-20" />
+        <div
+          id="layout-setting"
+          class="f-c-c rounded-4 bg-primary p-8 cursor-pointer"
+          @click="modalRef.open()"
+        >
+          <i class="i-fe:settings bg-white text-20" />
         </div>
       </template>
       Thiết lập bố cục
     </n-tooltip>
 
-    <MeModal ref="modalRef" title="Thiết lập bố cục" :show-footer="false" width="600px">
+    <MeModal
+      ref="modalRef"
+      title="Thiết lập bố cục"
+      :show-footer="false"
+      width="600px"
+    >
       <n-space justify="space-between">
-        <div class="flex-col cursor-pointer justify-center" @click="appStore.setLayout('simple')">
+        <div
+          class="flex-col cursor-pointer justify-center"
+          @click="appStore.setLayout('simple')"
+        >
           <div class="flex">
             <n-skeleton :width="20" :height="60" />
             <div class="ml-4">
@@ -35,7 +39,10 @@
             Tối giản
           </n-button>
         </div>
-        <div class="flex-col cursor-pointer justify-center" @click="appStore.setLayout('normal')">
+        <div
+          class="flex-col cursor-pointer justify-center"
+          @click="appStore.setLayout('normal')"
+        >
           <div class="flex">
             <n-skeleton :width="20" :height="60" />
             <div class="ml-4">
@@ -52,7 +59,10 @@
             Thông thường
           </n-button>
         </div>
-        <div class="flex-col cursor-pointer justify-center" @click="appStore.setLayout('full')">
+        <div
+          class="flex-col cursor-pointer justify-center"
+          @click="appStore.setLayout('full')"
+        >
           <div class="flex">
             <n-skeleton :width="20" :height="60" />
             <div class="ml-4">
@@ -69,7 +79,10 @@
             Đầy đủ
           </n-button>
         </div>
-        <div class="flex-col cursor-pointer justify-center" @click="appStore.setLayout('empty')">
+        <div
+          class="flex-col cursor-pointer justify-center"
+          @click="appStore.setLayout('empty')"
+        >
           <div class="flex">
             <n-skeleton :width="20" :height="60" />
             <div class="ml-4">
@@ -87,17 +100,19 @@
         </div>
       </n-space>
       <p class="mt-16 opacity-50">
-        Lưu ý: Thiết lập này chỉ có hiệu lực đối với các trang không thiết lập layout hoặc thiết lập theo hệ thống, layout được thiết lập trong menu có độ ưu tiên cao nhất
+        Lưu ý: Thiết lập này chỉ có hiệu lực đối với các trang không thiết lập
+        layout hoặc thiết lập theo hệ thống, layout được thiết lập trong menu có
+        độ ưu tiên cao nhất
       </p>
     </MeModal>
   </div>
 </template>
 
 <script setup>
-import { MeModal } from '@/components'
-import { useModal } from '@/composables'
-import { useAppStore } from '@/store'
+import { MeModal } from "@/components";
+import { useModal } from "@/composables";
+import { useAppStore } from "@/store";
 
-const appStore = useAppStore()
-const [modalRef] = useModal()
+const appStore = useAppStore();
+const [modalRef] = useModal();
 </script>
